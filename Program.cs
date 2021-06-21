d82c29258891d96a04bf36fb9d81d3dbdadc0836
Hassan Mohamed <hassanalimohamed1@gmail.com>
2021-06-21 12:29:40 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdmissionModularized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gpa;
+            int testscore;
+            string inputString;
+            Console.Write("Enter grade point average :");
+            inputString = Console.ReadLine();
+            gpa = Convert.ToDouble(inputString);
+            Console.Write("Enter test score: ");
+            inputString = Console.ReadLine();
+            testscore = Convert.ToInt32(inputString);
+            Console.WriteLine($"{AdmissionStatus(gpa, testscore)}");
+
+        }
+
+        private static string AdmissionStatus(double gpa, int testscore) {
+            const double MINGPA = 3.0;
+            const int ADMIN1 = 60, ADMIN2 = 80;
+
+            if (gpa >= MINGPA)
+            {
+                if (testscore >= ADMIN1)
+                {
+                    return("Accept");
+              
+                }
+                else
+                {
+                    return ("Reject");
+                }
+            }
+
+            else
+            {
+
+                if (testscore >= ADMIN2)
+                {
+                    return ("Accept");
+                }
+                else
+                {
+                    return ("Reject");
+                }
+
+            }
+
+        }
+    }
+}
